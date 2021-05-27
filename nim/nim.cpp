119878d397ea1184a18c56155514128d6f642546
BjornKlarstrom <71707050+BjornKlarstrom@users.noreply.github.com>
2021-05-27 19:47:01 +0200

    adds old code and .gitinores to folders

--- nim/nim.cpp
+++ nim/nim.cpp
@@ -1,7 +1,44 @@
 #include <iostream>
+#include <ctime>
+
+int GetPlayerChoice()
+{
+    return 1;
+}
+
+int GetAiChoice()
+{
+    return 2;
+}
+
 
 int main()
 {
-    std::cout << "Hello Nim!\n";
-}
+    const int MatchesToStartWith = 24;
+    int MatchesLeft = MatchesToStartWith;
+    bool IsPlayerOnesTurn = rand() % 2;
+    srand(time(NULL));
 
+    std::cout << "\n               -----------------------------\n";
+    std::cout << "                        WELCOME TO\n";
+    std::cout << "                      THE N.I.M GAME!";
+    std::cout << "\n               -----------------------------\n";
+    std::cout << "\n     A game about picking matches and avoiding the last one";
+    std::cout << "\n     Picking the last one will result in death\n\n";
+
+
+    std::cout << "\n     ";
+    for (int i = 0; i < MatchesLeft; i++)
+    {
+        std::cout << "|";
+    }
+
+    while (MatchesLeft > 0)
+    {
+        IsPlayerOnesTurn = !IsPlayerOnesTurn;
+        MatchesLeft -= IsPlayerOnesTurn ? GetPlayerChoice() : GetAiChoice();
+    }
+
+    std::cout << "\n\n";
+    return 0;
+}
