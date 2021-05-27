6a1f881eddcbbccc9b2f15f269dd7ef1a042c28e
BjornKlarstrom <71707050+BjornKlarstrom@users.noreply.github.com>
2021-05-28 00:25:29 +0200

    adds Grid and players

--- tictactoe/tictactoe.cpp
+++ tictactoe/tictactoe.cpp
@@ -1,20 +1,46 @@
-// tictactoe.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+bool ShowPlayerOne(int* PlayerOne, int index)
+{
+    return PlayerOne[index] > 0;
+}
+
+bool ShowPlayerTwo(int* PlayerTwo, int index)
+{
+    return PlayerTwo[index] > 0;
+}
+
+void ShowGrid(int* PlayerOne, int* PlayerTwo)
+{
+    const int GridSize = 9;
+
+    for (int i = 0; i < GridSize; i++)
+    {
+        if (ShowPlayerOne(PlayerOne, i))
+        {
+            std::cout << " " << "x" << " ";
+        }
+        if (ShowPlayerTwo(PlayerTwo, i))
+        {
+            std::cout << " " << "o" << " ";
+        }
+        else
+            std::cout << " " << "i" << " ";
+
+        if (i == 2 || i == 5 || i == 8)
+        {
+            std::cout << "" << std::endl;
+            continue;
+        }
+
+        std::cout << "|";
+    }
+}
 
 int main()
 {
-    std::cout << "Hello TicTacToe!\n";
+    int PlayerOne[9] = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+    int PlayerTwo[9] = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+    ShowGrid(PlayerOne, PlayerTwo);
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
