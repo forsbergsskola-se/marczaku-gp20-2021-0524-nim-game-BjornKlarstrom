ffe4a6fcb90d22558735b23b3ab9e8fd1e6dc360
BjornKlarstrom <71707050+BjornKlarstrom@users.noreply.github.com>
2021-05-28 03:17:30 +0200

    adds setup for decimal to binary

--- binary-converter/binary-converter.cpp
+++ binary-converter/binary-converter.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
+void DecimalToBinary(int UserNumber)
+{
+    // 
+}
+
 int main()
 {
     std::cout << "Hello Binary Converter";
+
+    int UserNumber;
+    std::cout << "\n\nInput a number (positive): ";
+    std::cin >> UserNumber;
+    std::cout << UserNumber;
+
+    DecimalToBinary(UserNumber);
+
+    return 0;
 }
